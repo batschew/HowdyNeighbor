546401e633e05527fc5149206c8be96afdfe82b3
Evan Batsch <evan@refractlabs.com>
2021-03-28 18:21:20 -0400

    updated pages and removed migrations

--- HowdyNeighbor/Pages/MovingList.cshtml.cs
+++ HowdyNeighbor/Pages/MovingList.cshtml.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HowdyNeighbor.Authorization;
 using HowdyNeighbor.Data;
 using HowdyNeighbor.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace HowdyNeighbor.Pages
@@ -16,14 +18,37 @@ namespace HowdyNeighbor.Pages
         {
         }
 
-        public IList<ChecklistTask> ChecklistTask { get; set; }
+        public IList<ChecklistTask> ChecklistTasks { get; set; }
+
+        [BindProperty]
+        public ChecklistTask ChecklistTask { get; set; }
 
         public async Task OnGetAsync()
         {
             var checklistTasks = from task in Context.ChecklistTask select task;
             var currentUserId = UserManager.GetUserId(User);
             checklistTasks = checklistTasks.Where(task => task.OwnerID == currentUserId);
-            ChecklistTask = await checklistTasks.ToListAsync();
+            ChecklistTasks = await checklistTasks.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ChecklistTask.OwnerID = UserManager.GetUserId(User);
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(User, ChecklistTask, Operations.Create);
+            if (!isAuthorized.Succeeded)
+            {
+                return Forbid();
+            }
+
+            Context.ChecklistTask.Add(ChecklistTask);
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./MovingList");
         }
     }
 }
